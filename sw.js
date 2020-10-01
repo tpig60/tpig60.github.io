8a43f0dbdb3f13cb2572c81442e24ba7eaf57d51
Miguel Angel Ortiz Marin <miaortizma@unal.edu.co>
2020-09-30 19:35:12 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f460422583dd33273127.js"
+    "url": "webpack-runtime-1e3f9db9f846aff443bd.js"
   },
   {
     "url": "styles.04422fe2d279e1dcef99.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b69ef681bcdd3c2d2656320066de8f24"
+    "revision": "ab2c9bbd38f19dd21433b443e19ca2d3"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5a1ee105f05b3144b807.js"
